ee80ee68008d1dd9e69388af37898bc7393c5f41
Serpil Karamustafa <serpilkaramustafa1@gmail.com>
2025-04-17 01:00:32 +0300

    denem

--- src/deneme/diger.java
+++ src/deneme/diger.java
@@ -2,6 +2,6 @@ package deneme;
 
 public class diger {
     public static void main(String[] args) {
-        System.out.println("  ");
+        System.out.println(" Fulya Hocamızın projesi  ");
     }
 }
